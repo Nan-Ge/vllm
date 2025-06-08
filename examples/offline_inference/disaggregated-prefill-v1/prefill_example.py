e665294138dc0336dd2f67e9cbc2c8ce19cf0610
Haonan Wu <haonanwu@sjtu.edu.cn>
2025-06-08 16:52:35 +0800

    testing LMCache PD disaggregation example

--- examples/offline_inference/disaggregated-prefill-v1/prefill_example.py
+++ examples/offline_inference/disaggregated-prefill-v1/prefill_example.py
@@ -21,9 +21,9 @@ def main():
     sampling_params = SamplingParams(temperature=0, top_p=0.95, max_tokens=1)
 
     llm = LLM(
-        model="meta-llama/Llama-3.2-1B-Instruct",
+        model="facebook/opt-125m",
         enforce_eager=True,
-        gpu_memory_utilization=0.8,
+        gpu_memory_utilization=0.01,
         kv_transfer_config=KVTransferConfig(
             kv_connector="SharedStorageConnector",
             kv_role="kv_both",
@@ -31,7 +31,7 @@ def main():
         ),
     )  # , max_model_len=2048, max_num_batched_tokens=2048)
 
-    # 1ST generation (prefill instance)
+    # 1ST generation (prefill i?nstance)
     outputs = llm.generate(
         prompts,
         sampling_params,
--- vllm/distributed/kv_transfer/kv_transfer_state.py
+++ vllm/distributed/kv_transfer/kv_transfer_state.py
@@ -57,11 +57,11 @@ def ensure_kv_transfer_initialized(vllm_config: "VllmConfig") -> None:
     if vllm_config.kv_transfer_config is None:
         return
 
-    if (vllm_config.kv_transfer_config.is_kv_transfer_instance
-            and _KV_CONNECTOR_AGENT is None):
+    if vllm_config.kv_transfer_config.is_kv_transfer_instance and _KV_CONNECTOR_AGENT is None:
         if envs.VLLM_USE_V1:
             _KV_CONNECTOR_AGENT = KVConnectorFactory.create_connector_v1(
-                config=vllm_config, role=KVConnectorRole.WORKER)
+                config=vllm_config, role=KVConnectorRole.WORKER
+            )
         else:
             _KV_CONNECTOR_AGENT = KVConnectorFactory.create_connector_v0(
                 rank=get_world_group().rank,
